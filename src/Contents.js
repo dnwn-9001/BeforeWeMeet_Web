48a5b3a53b60fe27c2738c2ff50739f1a527efa1
dnwn-9001 <j10511@hanmail.net>
2022-07-06 02:17:31 +0900

    youtube

--- src/Contents.js
+++ src/Contents.js
@@ -1,7 +1,30 @@
 import "./Common.css";
 import styles from "./Contents.module.css";
+import axios from "axios";
+import React from "react";
 
 function Contents() {
+  // 유튜브 정보 요청
+  const [data, setData] = React.useState([]);
+  React.useEffect(() => {
+    axios
+      .get("http://localhost:8081/youtube")
+      .then((result) => {
+        const data = result.data;
+        // let titles = data["titleList"];
+        // let urls = data["urlList"];
+        // let imgs = data["imgList"];
+        console.log(data);
+        // data["titleList"].forEach((list) => {
+        //   console.log("list : " + list);
+        // });
+        setData(data);
+      })
+      .catch((error) => {
+        console.log("요청이 실패했습니다.");
+      });
+  }, []);
+
   return (
     <div className={styles.content}>
       <div className={styles.content__title}>
@@ -13,31 +36,8 @@ function Contents() {
           <option value="meerkat">미어캣</option>
         </select>
       </div>
+
       <div className={styles.content__box}>
-        <div className={styles.content__box__dtl}>
-          <img
-            className={styles.content__box__img}
-            src="images/thumbnail_youtube.png"
-            alt="썸네일"
-          />
-          <p className="content__box__title">title</p>
-        </div>
-        <div className={styles.content__box__dtl}>
-          <img
-            className={styles.content__box__img}
-            src="images/thumbnail_youtube.png"
-            alt="썸네일"
-          />
-          <p className="content__box__title">title</p>
-        </div>
-        <div className={styles.content__box__dtl}>
-          <img
-            className={styles.content__box__img}
-            src="images/thumbnail_youtube.png"
-            alt="썸네일"
-          />
-          <p className="content__box__title">title</p>
-        </div>
         <div className={styles.content__box__dtl}>
           <img
             className={styles.content__box__img}
--- src/Footer.js
+++ src/Footer.js
@@ -4,12 +4,12 @@ import styles from "./Footer.module.css";
 function Footer() {
   return (
     <div className={styles.footer}>
-      <div className="footer__dtl">
+      <div className={styles.footer__dtl}>
         <span>이용약관</span>
         <span>개인정보처리방침</span>
         <span>저작권안내</span>
       </div>
-      <div className="footer__rights">
+      <div className={styles.footer__rights}>
         <p>Before We Meet RIGHTS RESERVED.</p>
       </div>
     </div>

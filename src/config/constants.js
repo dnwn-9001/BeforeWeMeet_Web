e4143b1c969f751e1c00bedc593aed5b3450d284
dnwn-9001 <j10511@hanmail.net>
2022-07-13 19:23:40 +0900

    constants.js 수정

--- src/config/constants.js
+++ src/config/constants.js
@@ -5,5 +5,5 @@ export const API_URL =
 
 export const SOCKET_URL =
   process.env.NODE_ENV === "production"
-    ? "https://before-we-meet.herokuapp.com:" + process.env.PORT + 1
+    ? "https://before-we-meet.herokuapp.com"
     : "http://localhost:8082";
